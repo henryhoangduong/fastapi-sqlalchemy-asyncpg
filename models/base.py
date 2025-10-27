5b48c53f1850856bcf29c00733a6099e7bbc48c3
henryhoangduong <henryhoangduong@gmail.com>
2025-10-27 21:26:17 +0700

    base db model

---
+++ models/base.py
@@ -0,0 +1,57 @@
+from typing import Any
+from asyncpg import UniqueViolationError
+from fastapi import HTTPException, status
+from rotoger import Rotoger
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+
+class Base(DeclarativeBase):
+    id: Any
+    __name__: str
+
+    @declared_attr
+    def __tablename__(self) -> str:
+        return self.__name__.lower()
+
+    async def save(self, db_session: AsyncSession):
+        db_session.add(self)
+        await db_session.flush()
+        await db_session.refresh(self)
+        return self
+
+    async def delete(self, db_session: AsyncSession):
+        try:
+            await db_session.delete(self)
+            return True
+        except SQLAlchemyError as ex:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=repr(
+                    ex)
+            ) from ex
+
+    async def update(self, **kwargs):
+        try:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+            return True
+        except SQLAlchemyError as ex:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=repr(
+                    ex)
+            ) from ex
+
+    async def save_or_update(self, db_session: AsyncSession):
+        try:
+            db_session.add(self)
+            await db_session.flush()
+            return True
+        except IntegrityError as exception:
+            if isinstance(exception.orig, UniqueViolationError):
+                return await db_session.merge(self)
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail=repr(exception),
+                ) from exception
