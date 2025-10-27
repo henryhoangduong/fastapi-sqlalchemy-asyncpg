ac4885112fef562b28ebc248e849790a3d7ba3ce
henryhoangduong <henryhoangduong@gmail.com>
2025-10-27 22:15:23 +0700

    user schema

---
+++ schemas/user.py
@@ -0,0 +1,42 @@
+from uuid import UUID
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, SecretStr
+
+config = ConfigDict(from_attributes=True)
+
+
+class UserSchema(BaseModel):
+    model_config = config
+    email: EmailStr = Field(
+        title="User's email", description="User's email", examples=["john@domain.com"]
+    )
+    first_name: str = Field(
+        title="User's first name", description="User's first name", examples=["John"]
+    )
+    last_name: str = Field(
+        title="User's last name", description="User's last name", examples=["Doe"]
+    )
+    password: SecretStr = Field(
+        title="User's password",
+        description="User's password",
+        examples=["@SuperSecret123"],
+    )
+
+
+class UserResponse(BaseModel):
+    access_token: str = Field(
+        title="User's access token", description="User's access token"
+    )
+    token_type: str = Field(title="User's token type",
+                            description="User's token type")
+
+
+class UserLogin(BaseModel):
+    model_config = config
+    email: EmailStr = Field(
+        title="User's email", description="User's email", examples=["john@domain.com"]
+    )
+    password: SecretStr = Field(
+        title="User's password",
+        description="User's password",
+        examples=["@SuperSecret123"],
+    )
